5683bbd41f5c2620a892f9d1569b9631be2b2fa2
Emmanuel-Aggrey <aggrey.en@gmail.com>
2021-06-02 04:04:19 +0000

    .

--- restframework/admin.py
+++ restframework/admin.py
@@ -1,15 +1,15 @@
 from django.contrib import admin
-from import_export.admin import ImportExportModelAdmin,ImportExportMixin
-from .models import Item
+from import_export.admin import ImportExportModelAdmin
+
+from .models import Import
+
 # Register your models here.
 
 
 
 
-
 @admin.register(Item)
-class PersonAdmin(ImportExportModelAdmin):
+class ItemAdmin(ImportExportModelAdmin):
     list_display = ('brand','model_no','type_of_computer','assiend_end_user','status','ip_address')
     search_fields = ('name','electoralArea__name')
     list_filter = ['status','brand','type_of_computer']
-
