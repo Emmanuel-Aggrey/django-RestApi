2b03a9da904e288ef713eaa81005cb022bc0adc4
Emmanuel-Aggrey <aggrey.en@gmail.com>
2021-06-02 03:40:48 +0000

    .

--- restframework/admin.py
+++ restframework/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from import_export.admin import ImportExportModelAdmin,ImportExportMixin
 # Register your models here.
+
+
+
+
+
+@admin.register(Item)
+class PersonAdmin(ImportExportModelAdmin):
+    list_display = ('brand','model_no','type_of_computer','assiend_end_user','status','ip_address')
+    search_fields = ('name','electoralArea__name')
+    list_filter = ['status','brand','type_of_computer']
+
--- restframework/models.py
+++ restframework/models.py
@@ -27,3 +27,40 @@ class Person(models.Model):
 def create_auth_token(sender, instance=None, created=False, **kwargs):
     if created:
         Token.objects.create(user=instance)
+        
+        
+
+class Item(models.Model):
+    status=[
+        ['Active', 'Active'],
+        ['In_Active', 'In Active'],
+        ['Dead','Dead'],
+    ]
+
+    node=[
+        ['PC', 'PC'],
+        ['PHONE', 'PHONE'],
+        ['PRINTER','PRINTER'],
+        ['DLINK_SWITCH','DLINK SWITCH'],
+        ['CISCO_SWITCH','CISCO SWITCH'],
+    ]
+    note = models.CharField(max_length=200,null=True,blank=True,help_text='just not to confuse each other any descriptive text')
+    brand = models.CharField(max_length=200)
+    model_no = models.CharField(max_length=200,blank=True,null=True,help_text="wmic computersystem get model,name")
+    serial_no = models.CharField(max_length=200,null=True,blank=True,unique=True,help_text='wmic bios get serialnumber')
+    node_name= models.CharField(max_length=200,null=True,blank=True,help_text='wmic computersystem get name')
+    type_of_computer = models.CharField(max_length=200,blank=True,null=True,choices=node)
+    processor = models.CharField(max_length=200,null=True,blank=True)
+    ram_size = models.CharField(max_length=200,null=True,blank=True)
+    hdd_size = models.CharField(max_length=200,null=True,blank=True)
+    ip_address= models.GenericIPAddressField(unique=True,null=True,blank=True)
+    location = models.CharField(max_length=200,null=True,blank=True)
+
+    status =models.CharField(max_length=20,null=True,blank=True,choices=status)
+    assiend_end_user= models.CharField(max_length=200,null=True,blank=True)
+
+    def __str__(self):
+        return f'{self.brand} {self.brand}'
+
+
+   
